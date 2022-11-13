f282291efc52cd7a330ef50846f46b05d536ee28
Khoa Le <kl58277@uga.edu>
2022-11-13 01:17:43 -0500

    add logo and title

--- src/app.py
+++ src/app.py
@@ -1,5 +1,9 @@
 import tkinter as tk
 from settings import *
+from PIL import ImageTk, Image
+import pyglet
+
+pyglet.font.add_file("assets/punktypewriter.otf")
 
 class Steganography:
     def __init__(self):
@@ -8,5 +12,19 @@ class Steganography:
         self.window.geometry(f"{self.setting.width}x{self.setting.height}")
         self.window.iconbitmap(self.setting.icon)
         self.window.title(self.setting.title)
+        self.define_app_content()
+
     def run(self):
         self.window.mainloop()
+
+    def define_app_content(self):
+        self.title_in_app = tk.Label(self.window, 
+        text = self.setting.title, 
+        font=("punktypewriter", self.setting.title_font_size)).place(x=40,y=2)
+        self.logo = ImageTk.PhotoImage(Image.open(self.setting.icon))
+        self.logo_label = tk.Label(image=self.logo).place(x=7,y=4)
+
+
+
+
+
--- src/settings.py
+++ src/settings.py
@@ -1,7 +1,12 @@
+from PIL import ImageTk, Image
+
 class Setting():
     def __init__(self):
         self.width = 1000
         self.height = 750
         self.icon = "assets/hackingicon.ico"
         self.title = "Steganography Encrypter"
+        self.title_font_size = 20
+        
+
     
