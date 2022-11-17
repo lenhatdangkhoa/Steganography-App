65b9c665e1a394fbffde6a2469d69bdc238bb3db
Khoa Le <kl58277@uga.edu>
2022-11-16 22:52:15 -0500

    added button function

--- src/app.py
+++ src/app.py
@@ -1,6 +1,8 @@
 import tkinter as tk
 from settings import *
 from PIL import ImageTk, Image
+import os
+from tkinter import filedialog as fd
 
 class Steganography:
     def __init__(self):
@@ -20,16 +22,24 @@ class Steganography:
         font=("Century Gothic", self.setting.title_font_size)).place(x=40,y=2)
         self.logo = ImageTk.PhotoImage(Image.open(self.setting.icon))
         self.logo_label = tk.Label(image=self.logo).place(x=7,y=4)
-        self.button = tk.Button(self.window, text="Open", width = 15, command=None)
+        self.button = tk.Button(self.window, text="Open", width = 15, command=self.openImagePath)
         self.button.bind("<Enter>", self.on_enter)
         self.button.bind("<Leave>", self.on_leave)
         self.button.place(x=850,y = 15)
 
+    # Change color of button when cursor is on top
     def on_enter(self, e):
         self.button.config(background="red", foreground="black")
 
+    # Change color of button back to normal when cursor leaves
     def on_leave(self, e):
         self.button.config(background="SystemButtonFace", foreground="black")
+
+    # Get file path from the user <3
+    def openImagePath(self):
+        image_path = fd.askopenfilename()
+        print(image_path)
+        
 
 
 
