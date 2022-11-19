e9f543d71bdec7774e7b0f823437c5bb434d526f
Khoa Le <kl58277@uga.edu>
2022-11-19 14:23:46 -0500

    test binary conversion

--- src/tester.py
+++ src/tester.py
@@ -4,10 +4,20 @@ from PIL import ImageTk, Image
 import os
 from tkinter import filedialog as fd
 
-image = Image.open("assets/background.png")
 
-image.putalpha(45)
-image.save("assets/edited_background.png")
-image.show()
+image = Image.open("assets/test_image.png")
 
+word = "abc"
+binary_string = ""
+val = image.getpixel((0,0))
 
+print(val[0])
+"""
+for i in range(len(word)):
+    char_ascii = ord(word[i])
+    binary_string += "0" + bin(char_ascii)[2:]
+
+for i in range(0,len(binary_string), 8):
+    print(chr(int(binary_string[i:i+8], 2)))
+
+"""
